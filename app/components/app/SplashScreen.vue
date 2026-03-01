c3091052bed19b194fe13d1ce1f7071c9d67f12e
oyedejioyewole <57059138+oyedejioyewole@users.noreply.github.com>
2026-03-01 04:49:55 +0100

    feat: ✨ Added animated SVG logo and splash screen.
    Note: The splash screen only shows up on pages with tasks and dismounts
    on client-side navigation.

---
+++ app/components/app/SplashScreen.vue
@@ -0,0 +1,23 @@
+<template>
+  <section
+    class="flex min-h-screen flex-col items-center justify-center gap-y-6"
+  >
+    <AppBranding
+      ref="brand-logo"
+      class="size-30 rounded-lg blur-sm"
+      is-loading
+    />
+
+    <ProseP>
+      Loading:
+      <span class="font-display"
+        >{{ Math.ceil((numberOfCompletedTasks / numberOfTasks) * 100) }}%</span
+      >
+    </ProseP>
+  </section>
+</template>
+
+<script lang="ts" setup>
+const props = defineProps<{ numberOfTasks: number }>();
+const { numberOfCompletedTasks } = useTasks();
+</script>
