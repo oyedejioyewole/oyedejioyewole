9f5a6688a0e2d4412dea0d7d6e51a625dd599c29
oyedejioyewole <57059138+oyedejioyewole@users.noreply.github.com>
2026-03-05 14:53:01 +0100

    refactor: ✏️ Consolidated sidebars into single navigation and footer.

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,50 +1,26 @@
 <template>
-  <Html lang="en" />
+  <Html data-scroll-container lang="en" />
 
   <Body
     class="bg-primary-100 dark:bg-primary-900 text-primary-700 dark:text-primary-300 selection:bg-primary-500/50 cursor-crosshair overflow-x-hidden"
-    data-scroll-container
   />
 
-  <ClientOnly>
-    <NuxtLoadingIndicator
-      :color="
-        $colorMode.value === 'light'
-          ? 'var(--color-primary-700)'
-          : 'var(--color-primary-300)'
-      "
-    />
-  </ClientOnly>
-
-  <AppEffectsCursorTracking v-show="showCursorFx" />
+  <AppEffectsCursorTracking />
 
   <main class="grid min-h-screen grid-cols-5">
     <!-- Left Sidebar -->
-    <AppLeftSidebar />
+    <AppNavigation />
 
     <!-- Content -->
     <main
-      :class="[
-        'ease-in-out-circ col-span-3 mx-auto w-[90%] transition-all duration-300',
-        hasRightSidebarBeenHovered ? 'xl:w-full' : 'xl:w-[125%]',
-      ]"
+      class="ease-in-out-circ col-span-4 mx-auto w-[90%] transition-all duration-300 xl:w-full"
     >
       <slot />
     </main>
-
-    <!-- Right Sidebar -->
-    <AppRightSidebar
-      v-model:cursorFx="enableCursorFx"
-      v-model:hovered="hasRightSidebarBeenHovered"
-    />
   </main>
 </template>
 
 <script lang="ts" setup>
-const enableCursorFx = shallowRef(true);
-const showCursorFx = computed(() => enableCursorFx.value);
-const hasRightSidebarBeenHovered = shallowRef(false);
-
 useHead({
   titleTemplate: "%s · Oyedeji Oyewole",
 });
