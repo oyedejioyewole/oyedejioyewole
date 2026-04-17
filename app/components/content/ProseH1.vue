b94d458d37f68cec186fb65c62c710b05705fff9
oyedejioyewole <57059138+oyedejioyewole@users.noreply.github.com>
2026-04-17 22:07:43 +0100

    style: 🎨 update ProseH1 and ProseH2 typography for responsive sizing.

--- app/components/content/ProseH1.vue
+++ app/components/content/ProseH1.vue
@@ -1,5 +1,8 @@
 <template>
-  <h1 class="font-display text-6xl tracking-tighter" :id="$props.id">
+  <h1
+    class="font-display text-5xl tracking-tighter xl:text-6xl"
+    :id="$props.id"
+  >
     <a v-if="generate" :href="`#${$props.id}`">
       <slot />
     </a>
