f4d7ed93d333d8e7657be0b78f5d92029394b6bc
oyedejioyewole <57059138+oyedejioyewole@users.noreply.github.com>
2026-04-29 19:57:02 +0100

    feat: ✨ add `Languages` component to /projects page.

---
+++ app/components/pages/projects/Languages.vue
@@ -0,0 +1,25 @@
+<template>
+  <motion.button
+    class="flex w-fit justify-center gap-x-2 rounded-xl border-current/30 bg-current/10 px-5 py-3 backdrop-blur-lg transition-all duration-300 hover:gap-x-4 hover:border-current/70 focus:gap-x-4"
+    data-snap-cursor
+    :while-press="{ scale: 0.9 }"
+  >
+    <NuxtImg
+      v-for="entry of $props.entries"
+      :key="entry"
+      class="size-6"
+      provider="none"
+      :src="`https://svgl.app/library/${entry}.svg`"
+    />
+  </motion.button>
+</template>
+
+<script lang="ts" setup>
+import { motion } from "motion-v";
+
+interface TechStackProps {
+  entries: string[];
+}
+
+const props = defineProps<TechStackProps>();
+</script>
--- content/projects.md
+++ content/projects.md
@@ -2,6 +2,14 @@
 title: My projects
 ---
 
-:PagesProjects
+::PagesProjects
+
+# My projects.
+
+These are some of the languages/frameworks I use in my projects.
+
+:PagesProjectsLanguages{:entries='["python","tailwindcss","typescript","vue","nuxt","tauri"]'}
+
+::
 
 :PagesProjectsStatistics
