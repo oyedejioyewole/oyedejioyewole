47f745e551a96cc712073012f1a4e9f72c9f000f
oyedejioyewole <57059138+oyedejioyewole@users.noreply.github.com>
2026-06-12 23:54:21 +0100

    refactor: 🔄 migrate app shell from Nuxt to Astro

---
+++ src/utils/path-transition.ts
@@ -0,0 +1,35 @@
+import { animate } from "motion";
+
+export const pathTransition = document.querySelector<SVGElement>(
+  "svg#path-transition",
+)!;
+
+export async function exitSequence() {
+  await animate(
+    pathTransition.querySelector("path"),
+    {
+      pathLength: [1, 0],
+      pathOffset: [0, 1],
+      ...(window.innerWidth >= 1280 && { strokeWidth: [250, 2] }),
+    },
+    { duration: 1 },
+  );
+
+  pathTransition.classList.add("invisible");
+}
+
+export async function enterSequence(onComplete?: () => Promise<void>) {
+  pathTransition.classList.remove("invisible");
+
+  await animate(
+    pathTransition.querySelector("path"),
+    {
+      pathLength: [0, 1],
+      pathOffset: [0, 0],
+      ...(window.innerWidth >= 1280 && { strokeWidth: [2, 250] }),
+    },
+    { duration: 1 },
+  );
+
+  if (onComplete) await onComplete();
+}
